b608ef42a98780e56ef02b0151ae93133c79af04
ben <phas0ruk@gmail.com>
2022-05-18 16:49:42 -0700

    update rpc url to kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.0",
   networks: {
-            rinkeby: {
-                  url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            kovan: {
+                  url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
                   accounts: [process.env.PRIVATE_KEY]
                   }
   }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -34,8 +34,8 @@ async function main() {
   const bondContract = await Bond.deploy(
     "0xF0d7d1D47109bA426B9D8A3Cde1941327af1eea3", //host contract address
     "0xECa8056809e7e8db04A8fF6e4E82cD889a46FE2F", //cfa contract address
-    "0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1", //Kovan Ethx super token
-    1000000000000000000,                          // funding target in wei 
+    "0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1", //accpeted token, Kovan Ethx super token
+    100000000000000000,                          // funding target in wei 
     1000,                                         // interest rate in basis points
     365);                                         // loan term, days
 

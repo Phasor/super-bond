e9106210fb4168e791c9b11ebfc7b51587be3907
ben <phas0ruk@gmail.com>
2022-05-20 12:02:49 -0700

    added dedhardhat

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,11 +1,11 @@
 
-const fundingTarget = ethers.BigNumber.from("10000000000000000"); // 0.01 eth
+//const fundingTarget = ethers.BigNumber.from("100000000000000000000"); 
 
 module.exports = [
     "0xF0d7d1D47109bA426B9D8A3Cde1941327af1eea3", //host contract address
     "0xECa8056809e7e8db04A8fF6e4E82cD889a46FE2F", //cfa contract address
-    "0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1", //accpeted token, Kovan Ethx super token
-    fundingTarget,                          // funding target in wei 
+    "0x2eb320e2100a043401e3b3b132d4134f235a6a04", //accpeted token, Kovan USDC super token, fUSDC
+    100,                          // funding target in wei 
     1000,                                         // interest rate in basis points
     365
   ];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,13 +11,13 @@ async function main() {
 
   // We get the contract to deploy
     const Bond = await hre.ethers.getContractFactory("Bond");
-    const fundingTarget = ethers.BigNumber.from("10000000000000000"); // 0.01 eth
+    //const fundingTarget = ethers.BigNumber.from("10000000000000000"); // 0.01 eth
 
     const bondContract = await Bond.deploy(
-    "0xF0d7d1D47109bA426B9D8A3Cde1941327af1eea3", //host contract address
-    "0xECa8056809e7e8db04A8fF6e4E82cD889a46FE2F", //cfa contract address
-    "0x2eb320e2100a043401e3b3b132d4134f235a6a04", //accpeted token, Kovan USDC super token, fUSDC
-    fundingTarget,                          // funding target in wei 
+    "0xF0d7d1D47109bA426B9D8A3Cde1941327af1eea3", // host contract address
+    "0xECa8056809e7e8db04A8fF6e4E82cD889a46FE2F", // cfa contract address
+    "0x2eb320e2100a043401e3b3b132d4134f235a6a04", // accpeted token, Kovan USDC super token, fUSDC
+    100,                                          // funding target  
     1000,                                         // interest rate in basis points
     365);                                         // loan term, days
 

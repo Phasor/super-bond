156cd43359e638381ae988494f6b6892c2ef71cb
ben <phas0ruk@gmail.com>
2022-05-26 03:43:03 -0700

    connect button works

--- my-app/src/components/Connect.js
+++ my-app/src/components/Connect.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useState } from "react";
 import '../css/Connect.css';
-import '../css/Connect.css';
+import { ethers } from "ethers";
+
 
 export default function Connect() {
+  const [address, setAddress] = useState();
+  const [shortAddress, setShortAddress] = useState();
+
+  // A Web3Provider wraps a standard Web3 provider, which is
+  // what MetaMask injects as window.ethereum into each page
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  // The MetaMask plugin also allows signing transactions to
+  // send ether and pay to change state within the blockchain.
+  // For this, you need the account signer...
+  const signer = provider.getSigner()
+
+  // MetaMask requires requesting permission to connect users accounts
+  const requestConnection = async () => {
+    await provider.send("eth_requestAccounts", []);
+    const connectedAddress = await signer.getAddress();
+    const shortAddress = connectedAddress.substring(0, 7);
+    setAddress(connectedAddress);
+    setShortAddress(shortAddress);
+  } 
+
   return (
-    <button class="connect-button">Connect</button>
+    <>
+      {!address && <button class="connect-button" onClick={requestConnection}>Connect</button>}
+      {address && <button class="connect-button" onClick={() => window.location.reload(false)}>{shortAddress}...</button>}
+    </>
   )
 }

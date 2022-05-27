2a9d93898f80834ecd4fdf2bf3c1988812cd8620
ben <phas0ruk@gmail.com>
2022-05-26 20:31:47 -0700

    add borrow form

--- my-app/src/components/Borrow.js
+++ my-app/src/components/Borrow.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../css/Borrow.css';
+
 
 export default function Borrow() {
+  const [amountToRaise, setAmountToRaise] = useState();
+  const [rate, setRate] = useState();
+  const [term, setTerm] = useState();
+
   return (
-      <>
-        <div>Borrowing Info Working</div>
-        <div>Borrowing Info Working</div>
-        <div>Borrowing Info Working</div>
-        <div>Borrowing Info Working</div>
-    </>
+      <div className='borrow-container'>
+        <form className='borrow-form'>
+            <div className="form-row">
+              <label htmlFor="">Amount to Raise ($)</label>
+              <input type="text" 
+                  required
+                  value={amountToRaise}
+                  onChange={(e) => {setAmountToRaise(e.target.value)}}
+              />
+          </div>
+            <div className="form-row">
+              <label htmlFor="">Interest Paid Annually (%)</label>
+              <input type="text" 
+                  required
+                  value={rate}
+                  onChange={(e) => {setRate(e.target.value)}}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="">Loan Term (days)</label>
+              <input type="text" 
+                  required
+                  value={term}
+                  onChange={(e) => {setTerm(e.target.value)}}
+              />
+            </div>
+       </form>
+    </div>
   )
 }
